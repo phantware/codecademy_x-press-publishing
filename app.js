a154c3c2f622d1fc3910f0ff7ddb0bf081238447
phantware <phantmoney2011@gmail.com>
2020-05-13 13:12:16 +0100

    unused variables removed

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
-const sqlite3 = require("sqlite3");
 const apiRouter = require("./server/api");
 
 const app = express();
@@ -11,10 +10,6 @@ app.use(morgan("tiny"));
 app.use(cors());
 
 const PORT = process.env.PORT || 4000;
-
-const db = new sqlite3.Database(
-  process.env.TEST_DATABASE || "./database.sqlite"
-);
 
 app.use("/api", apiRouter);
 
